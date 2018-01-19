d1330a88a839dade44c2034d2fee66604f57079f
Prithvi Raju <nprithviraj24@gmail.com>
2018-01-20 00:42:16 +0700

    Inverse curve of Sine added.

---
+++ Shapes/Inverse-SINE.cpp
@@ -0,0 +1,113 @@
+/////////////////////////////////////////////////////////////////////         
+///
+// Interaction:
+// Press +/- to increase/decrease the number of vertices of the loop. 
+//
+// Sumanta Guha.
+///////////////////////////////////////////////////////////////////// 
+
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h> 
+
+#define PI 3.14159265358979324
+
+// Globals.
+static float X = 50.0; // X-coordinate of center of circle.
+static float Y = 50.0; // Y-coordinate of center of circle.
+static int numVertices = 5; // Number of vertices on circle.
+
+// Drawing routine.
+void drawScene(void)
+{
+	float t = 0; // Angle parameter.
+	int i;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Draw a line loop with vertices at equal angles apart on a circle
+	// with center at (X, Y) and radius R, The vertices are colored randomly.
+	glBegin(GL_LINE_STRIP);
+	for (i = 0; i < numVertices; ++i)
+	{
+		glColor3f((float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX);
+		glVertex3f(X + t*7, Y+ sin(-t)*7, 0.0);
+		t += 2 * PI / numVertices;
+	}
+	glEnd();
+
+	glFlush();
+}
+
+// Initialization routine.
+void setup(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+}
+
+// OpenGL window reshape routine.
+void resize(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+// Keyboard input processing routine.
+void keyInput(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		exit(0);
+		break;
+	case '+':
+		numVertices++;
+		glutPostRedisplay();
+		break;
+	case '-':
+		if (numVertices > 3) numVertices--;
+		glutPostRedisplay();
+		break;
+	default:
+		break;
+	}
+}
+
+// Routine to output interaction instructions to the C++ window.
+void printInteraction(void)
+{
+	std::cout << "Interaction:" << std::endl;
+	std::cout << "Press +/- to increase/decrease the number of vertices on the circle." << std::endl;
+}
+
+// Main routine.
+int main(int argc, char **argv)
+{
+	printInteraction();
+	glutInit(&argc, argv);
+
+	glutInitContextVersion(4, 3);
+	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(1000, 1000);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("circle.cpp");
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(resize);
+	glutKeyboardFunc(keyInput);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	setup();
+
+	glutMainLoop();
+}
